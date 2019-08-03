d7964601b72b4e3a4c07748d547c2364118d18fc
Talha <talhaayar53@gmail.com>
2019-08-03 14:22:35 +0300

    Initial Commit

---
+++ picturesInTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  picturesInTable
+//
+//  Created by TALHA AYAR on 3.08.2019.
+//  Copyright © 2019 Talha Ayar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

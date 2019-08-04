feef6cd9d8df00b5ec1b583e6b2b5a0d7458710b
Talha <talhaayar53@gmail.com>
2019-08-04 10:56:57 +0300

    First commit

---
+++ picturesInTable/DetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailViewController.swift
+//  picturesInTable
+//
+//  Created by TALHA AYAR on 3.08.2019.
+//  Copyright © 2019 Talha Ayar. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
